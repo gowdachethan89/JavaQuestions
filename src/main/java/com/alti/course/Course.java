d207207d3b31761515b698295eba09e7e99d100b
gowdachethan89 <chethan.ittasandra@gmail.com>
2026-03-31 15:20:01 +0530

    Course workout problem updating

---
+++ src/main/java/com/alti/course/Course.java
@@ -0,0 +1,26 @@
+package com.alti.course;
+
+import java.util.*;
+
+class Course {
+    /* Data about a particular course. */
+    public String title;  // The name of the obstacle course
+    public int obstacleCount;  // The number of obstacles in the course
+
+    public Course(String courseTitle, int obstacles) {
+        title = courseTitle;
+        obstacleCount = obstacles;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if(!(o instanceof Course)) { return false; }
+        Course c = (Course) o;
+        return c.title == this.title && c.obstacleCount == this.obstacleCount;
+    }
+
+    @Override
+    public int hashCode() {
+        return (title == null ? 0 : title.hashCode()) * obstacleCount;
+    }
+}
