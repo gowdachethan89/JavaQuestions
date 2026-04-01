d2bfedd023bd070e05b90cdd95940f6f1e8c11f6
gowdachethan89 <chethan.ittasandra@gmail.com>
2026-04-01 15:24:28 +0530

    Library problem

---
+++ src/main/java/com/alti/library/Book.java
@@ -0,0 +1,13 @@
+package com.alti.library;
+
+class Book {
+    int id;
+    String title;
+    BookStatus status;
+
+    public Book(int id, String title) {
+        this.id = id;
+        this.title = title;
+        this.status = BookStatus.AVAILABLE;
+    }
+}
